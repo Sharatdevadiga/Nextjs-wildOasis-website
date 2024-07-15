7f61771aa7629c4448debc652f7082c464dfbb91
Sharath <sharathdevadiga210@gmail.com>
2024-07-15 15:28:24 +0530

    initial commit

---
+++ app/error.js
@@ -0,0 +1,21 @@
+"use client";
+
+// only rendering errors can be caught with this error boundary
+// this does not catch erroes from the rootLayout
+// to catch the errors in the rootLayout, use the global-error.js
+
+export default function Error({ error, reset }) {
+  return (
+    <main className="flex justify-center items-center flex-col gap-6">
+      <h1 className="text-3xl font-semibold">Something went wrong!</h1>
+      <p className="text-lg">ERROR!</p>
+
+      <button
+        className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
+        onClick={reset}
+      >
+        Try again
+      </button>
+    </main>
+  );
+}
